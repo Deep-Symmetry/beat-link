1594712a4d314dd75528d23e32f590376ef4d444
James Elliott <james@deepsymmetry.org>
2016-05-06 13:14:37 -0500

    Add package level introduction.

---
+++ src/main/java/org/deepsymmetry/beatlink/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * A library for synchronizing with beats from Pioneer DJ Link equipment.
+ *
+ * <p>Overview and installation instructions are found on the <a href="https://github.com/brunchboy/beat-link"
+ * target="_blank">Project page</a> on GitHub.</p>
+ *
+ * <p>This project is based on research performed with <a href="https://github.com/brunchboy/dysentery"
+ * target="_blank">dysentery</a>,
+ * and the <a href="https://github.com/brunchboy/dysentery/blob/master/doc/Analysis.pdf" target="_blank">packet
+ * analysis</a> resulting from that project (also available as
+ * <a href="https://github.com/brunchboy/dysentery/raw/master/doc/Analysis.pdf" target="_blank">downloadable
+ * PDF</a>).</p>
+ *
+ * @author  James Elliott
+ */
+package org.deepsymmetry.beatlink;
