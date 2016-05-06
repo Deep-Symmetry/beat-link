d9152194c74befebd30b2303b8d672150becf06c
James Elliott <james@deepsymmetry.org>
2016-05-05 22:19:13 -0500

    Implemented device detection.

--- src/main/java/org/deepsymmetry/beatlink/DeviceAnnouncement.java
+++ src/main/java/org/deepsymmetry/beatlink/DeviceAnnouncement.java
@@ -1,9 +1,10 @@
 package org.deepsymmetry.beatlink;
 
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 
 /**
- * Represents a device announcement packet seen on a DJ Link network.
+ * Represents a device announcement seen on a DJ Link network.
  *
  * @author James Elliott
  */
@@ -32,24 +33,23 @@ public class DeviceAnnouncement {
     /**
      * The packet data containing the device announcement.
      */
-    private final byte[] packet;
+    private final byte[] packetBytes;
 
     /**
      * Constructor simply sets all the immutable fields.
      *
-     * @param address the address on which this device announcement was seen.
-     * @param packet the device announcement packet data.
+     * @param packet the device anouncement packet that was received.
      */
-    public DeviceAnnouncement(InetAddress address, byte[] packet) {
-        if (packet.length != 54) {
+    public DeviceAnnouncement(DatagramPacket packet) {
+        if (packet.getLength() != 54) {
             throw new IllegalArgumentException("Device announcement packet must be 54 bytes long");
         }
-        this.address = address;
-        this.packet = new byte[54];
-        System.arraycopy(packet, 0, this.packet, 0, 54);
-        this.timestamp = System.currentTimeMillis();
-        this.name = new String(packet, 12, 20).trim();
-        this.number = Util.unsign(packet[36]);
+        address = packet.getAddress();
+        packetBytes = new byte[54];
+        System.arraycopy(packet.getData(), 0, packetBytes, 0, 54);
+        timestamp = System.currentTimeMillis();
+        name = new String(packetBytes, 12, 20).trim();
+        number = Util.unsign(packetBytes[36]);
     }
 
     /**
@@ -85,16 +85,21 @@ public class DeviceAnnouncement {
      */
     public byte[] getHardwareAddress() {
         byte[] result = new byte[6];
-        System.arraycopy(packet, 38, result, 0, 6);
+        System.arraycopy(packetBytes, 38, result, 0, 6);
         return result;
     }
 
     /**
      * The raw data bytes of the device announcement packet.
      */
-    public byte[] getPacket() {
+    public byte[] getPacketBytes() {
         byte[] result = new byte[54];
-        System.arraycopy(packet, 0, result, 0, 54);
+        System.arraycopy(packetBytes, 0, result, 0, 54);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DJ Link Device Announcement: Device " + number + ", name: " + name + ", address: " + address;
     }
 }
--- src/main/java/org/deepsymmetry/beatlink/DeviceListener.java
+++ src/main/java/org/deepsymmetry/beatlink/DeviceListener.java
@@ -1,6 +1,13 @@
 package org.deepsymmetry.beatlink;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Watches for devices to report their presence by broadcasting keep-alive packets on port 50000,
@@ -9,10 +16,18 @@ import java.net.DatagramSocket;
  */
 public class DeviceListener {
 
+    private static final Logger logger = Logger.getLogger(DeviceListener.class.getName());
+
     /**
      * The port on which devices send keep-alive messages to report their presence on the network.
      */
-    public static final int KEEPALIVE_PORT = 50000;
+    public static final int ANNOUNCEMENT_PORT = 50000;
+
+    /**
+     * The number of milliseconds after which we will consider a device to have disappeared if
+     * we have not received an announcement from it.
+     */
+    public static final int MAXIMUM_AGE = 5000;
 
     /**
      * The socket used to listen for keep-alive packets while we are active.
@@ -20,8 +35,94 @@ public class DeviceListener {
     private static DatagramSocket socket;
 
     /**
-     * Start listening for
+     * Check whether we are presently listening for device announcements.
+     *
+     * @return true if our socket is open and monitoring for DJ Link devices announcements on the network.
      */
+    public static synchronized boolean isActive() {
+        return socket != null;
+    }
 
+    /**
+     * Keep track of the announcements we have seen.
+     */
+    private static final Map<InetAddress, DeviceAnnouncement> devices = new HashMap<InetAddress, DeviceAnnouncement>();
 
+    /**
+     * Remove any device announcements that are so old that the device seems to have gone away.
+     */
+    private static void expireDevices() {
+        long now = System.currentTimeMillis();
+        Iterator<Map.Entry<InetAddress, DeviceAnnouncement>> it = devices.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<InetAddress, DeviceAnnouncement> entry = it.next();
+            if (now - entry.getValue().getTimestamp() > MAXIMUM_AGE) {
+                it.remove();
+            }
+        }
+    }
+
+    /**
+     * Start listening for device announcements and keeping track of the DJ Link devices visible on the network.
+     * If already listening, has no effect.
+     *
+     * @throws SocketException if the socket to listen on port 50000 cannot be created.
+     */
+    public static synchronized void start() throws SocketException {
+
+        if (!isActive()) {
+            socket = new DatagramSocket(ANNOUNCEMENT_PORT);
+
+            final byte[] buffer = new byte[512];
+            final DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            Thread receiver = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (isActive()) {
+                        try {
+                            socket.receive(packet);
+                        } catch (IOException e) {
+                            // Don't log a warning if the exception was due to the socket closing at shutdown.
+                            if (isActive()) {
+                                // We did not expect to have a problem; log a warning and shut down.
+                                logger.log(Level.WARNING, "Problem reading from DeviceAnnouncement socket, stopping", e);
+                                stop();
+                            }
+                        }
+                        try {
+                            if (packet.getLength() == 54) {  // Looks like the kind of packet we are watching for
+                                DeviceAnnouncement announcement = new DeviceAnnouncement(packet);
+                                devices.put(announcement.getAddress(), announcement);
+                            }
+                        } catch (Exception e) {
+                            logger.log(Level.WARNING, "Problem parsing DeviceAnnouncement packet", e);
+                        }
+                    }
+                }
+            });
+            receiver.setDaemon(true);
+            receiver.start();
+        }
+    }
+
+    /**
+     * Stop listening for device announcements. Also discards any announcements which had been received.
+     */
+    public static synchronized void stop() {
+        if (isActive()) {
+            socket.close();
+            socket = null;
+            devices.clear();
+        }
+    }
+
+    /**
+     * Get the list of devices which currently can be seen on the network.
+     *
+     * @return the DJ Link devices which have been heard from recently enough to be considered active.
+     */
+    public static List<DeviceAnnouncement> currentDevices() {
+        expireDevices();
+        return Collections.unmodifiableList(new ArrayList<DeviceAnnouncement>(devices.values()));
+    }
 }
