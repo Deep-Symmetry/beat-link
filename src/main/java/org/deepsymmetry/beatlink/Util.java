d4151db0428260a7f79122bea92dca2a32125475
James Elliott <james@deepsymmetry.org>
2016-05-06 13:14:14 -0500

    Add network comparison method.

--- src/main/java/org/deepsymmetry/beatlink/Util.java
+++ src/main/java/org/deepsymmetry/beatlink/Util.java
@@ -1,11 +1,14 @@
 package org.deepsymmetry.beatlink;
 
+import java.net.InetAddress;
+
 /**
  * Provides utility functions.
  *
  * @author James Elliott
  */
 public class Util {
+
     /**
      * Converts a signed byte to its unsigned int equivalent in the range 0-255.
      *
@@ -15,6 +18,36 @@ public class Util {
      */
     public static int unsign(byte b) {
         return b & 0xff;
+    }
+
+    /**
+     * Converts the bytes that make up an internet address into the corresponding integer value to make
+     * it easier to perform bit-masking operations on them.
+     *
+     * @param address an address whose integer equivalent is desired.
+     *
+     * @return the integer corresponding to that address.
+     */
+    public static long addressToLong(InetAddress address) {
+        long result = 0;
+        for (byte element : address.getAddress()) {
+            result = (result << 8) + unsign(element);
+        }
+        return result;
+    }
+
+    /**
+     * Checks whether two internet addresses are on the same subnet.
+     *
+     * @param prefixLength the number of bits within an address that identify the network.
+     * @param address1 the first address to be compared.
+     * @param address2 the second address to be compared.
+     *
+     * @return true if both addresses share the same network bits.
+     */
+    public static boolean sameNetwork(int prefixLength, InetAddress address1, InetAddress address2) {
+        int prefixMask = 0xffffffff & (-1 << (32 - prefixLength));
+        return (addressToLong(address1) & prefixMask) == (addressToLong(address2) & prefixMask);
     }
 
     /**
