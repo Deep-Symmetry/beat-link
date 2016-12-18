1a55b9fc1a16871da4ca7665c0ca39ac7e826ae8
James Elliott <jim@Alacrity.local>
2016-12-18 00:54:07 -0600

    Support new, longer CDJ status packets.
    Pioneer has added a few more bytes, again!

--- src/main/java/org/deepsymmetry/beatlink/CdjStatus.java
+++ src/main/java/org/deepsymmetry/beatlink/CdjStatus.java
@@ -294,8 +294,8 @@ public class CdjStatus extends DeviceUpdate {
      */
     public CdjStatus(DatagramPacket packet) {
         super(packet, "CDJ status", packet.getLength());
-        if (packetBytes.length != 208 && packetBytes.length != 212 && packetBytes.length != 284) {
-            throw new IllegalArgumentException("CDJ status packet must be 208, 212, or 284 bytes long");
+        if (packetBytes.length != 208 && packetBytes.length != 212 && packetBytes.length != 284 && packetBytes.length != 292) {
+            throw new IllegalArgumentException("CDJ status packet must be 208, 212, 284, or 292 bytes long");
         }
         trackSourcePlayer = packetBytes[40];
         trackSourceSlot = findTrackSourceSlot();
--- src/main/java/org/deepsymmetry/beatlink/VirtualCdj.java
+++ src/main/java/org/deepsymmetry/beatlink/VirtualCdj.java
@@ -308,7 +308,8 @@ public class VirtualCdj {
         int kind = packet.getData()[10];
         if (length == 56 && kind == 0x29 && Util.validateHeader(packet, 0x29, "Mixer Status")) {
             return new MixerStatus(packet);
-        } else if ((length == 212 || length == 208 || length == 284) && kind == 0x0a && Util.validateHeader(packet, 0x0a, "CDJ Status")) {
+        } else if ((length == 212 || length == 208 || length == 284 || length == 292) &&
+                kind == 0x0a && Util.validateHeader(packet, 0x0a, "CDJ Status")) {
             return new CdjStatus(packet);
         }
         logger.warn("Unrecognized device update packet with length " + length + " and kind " + kind);
