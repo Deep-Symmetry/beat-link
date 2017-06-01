d105df7dda9365d3ac147dbe845fc3ddcedcebcb
James Elliott <james@deepsymmetry.org>
2017-05-31 22:46:37 -0500

    Precompute and cache the beat grid values.
    These are going to be used for fast computations, so let's make that
    efficient.

--- src/main/java/org/deepsymmetry/beatlink/BeatGrid.java
+++ src/main/java/org/deepsymmetry/beatlink/BeatGrid.java
@@ -35,9 +35,14 @@ public class BeatGrid {
     public final int beatCount;
 
     /**
-     * Holds the actual bytes of the grid, for calculating and reporting information about the beats in it.
+     * Holds the reported musical count of each beat.
      */
-    private final byte[] gridBytes;
+    private final int[] beatWithinBarValues;
+
+    /**
+     * Holds the reported start time of each beat in milliseconds.
+     */
+    private final long[] timeWithinTrackValues;
 
     /**
      * Constructor for when reading from the network.
@@ -55,23 +60,37 @@ public class BeatGrid {
      */
     public BeatGrid(ByteBuffer buffer) {
         rawData = buffer;
-        gridBytes = new byte[rawData.remaining()];
+        final byte[] gridBytes = new byte[rawData.remaining()];
         rawData.get(gridBytes);
         beatCount = (gridBytes.length - 20) / 16;
+        beatWithinBarValues = new int[beatCount];
+        timeWithinTrackValues = new long[beatCount];
+        for (int beatNumber = 0; beatNumber < beatCount; beatNumber++) {
+            final int base = 20 + beatNumber * 16;  // Data for the current beat starts here
+            beatWithinBarValues[beatNumber] = Util.unsign(gridBytes[base]);
+            // For some reason, unlike every other number in the protocol, beat timings are little-endian
+            timeWithinTrackValues[beatNumber] = 0;
+            for (int offset = base + 7; offset >= base + 4; offset--) {
+                timeWithinTrackValues[beatNumber] = (timeWithinTrackValues[beatNumber] << 8) +
+                        Util.unsign(gridBytes[offset]);
+            }
+        }
     }
 
     /**
      * Calculate where within the beat grid array the information for the specified beat can be found.
+     * Yes, this is a super simple calculation; the main point of the method is to provide a nice exception
+     * when the beat is out of bounds.
      *
      * @param beatNumber the beat desired
      *
-     * @return the offset of the start of that beat information within the bytes of the beat grid
+     * @return the offset of the start of our cache arrays for information about that beat
      */
     private int beatOffset(int beatNumber) {
         if (beatNumber < 1 || beatNumber > beatCount) {
-            throw new IllegalArgumentException("number must be between 1 and " + beatCount);
+            throw new IndexOutOfBoundsException("beatNumber must be between 1 and " + beatCount);
         }
-        return 20 + (beatNumber - 1) * 16;
+        return beatNumber - 1;
     }
 
     /**
@@ -84,18 +103,13 @@ public class BeatGrid {
      * @throws IllegalArgumentException if {@code number} is less than 1 or greater than {@code beatCount}
      */
     public long getTimeWithinTrack(int beatNumber) {
-        final int offset = beatOffset(beatNumber);
-        long result = 0;
-        // For some reason, unlike every other number in the protocol, these are little-endian.
-        for (int i = offset + 7; i >= offset + 4; i--) {
-            result = (result << 8) + Util.unsign(gridBytes[i]);
-        }
-        return result;
+        return timeWithinTrackValues[beatOffset(beatNumber)];
     }
 
     /**
-     * Returns the musical count of the specified beat, a number from 1 to 4, where 1 is the down beat, or the start
-     * of a new measure.
+     * Returns the musical count of the specified beat, represented by <i>B<sub>b</sub></i> in Figure 11 of
+     * the <a href="https://github.com/brunchboy/dysentery/blob/master/doc/Analysis.pdf">Packet Analysis document</a>.
+     * A number from 1 to 4, where 1 is the down beat, or the start of a new measure.
      *
      * @param beatNumber the number of the beat of interest, must fall within the range 1..beatcount
      *
@@ -103,7 +117,7 @@ public class BeatGrid {
      *
      * @throws IllegalArgumentException if {@code number} is less than 1 or greater than {@code beatCount}
      */
-    public int getPositionWithinBar(int beatNumber) {
-        return gridBytes[beatOffset(beatNumber)];
+    public int getBeatWithinBar(int beatNumber) {
+        return beatWithinBarValues[beatOffset(beatNumber)];
     }
 }
