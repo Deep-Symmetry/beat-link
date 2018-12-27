d1dc6be3af2f40b193fa7f174c6cf512fca82dc5
James Elliott <james@deepsymmetry.org>
2018-12-26 23:50:03 -0600

    Separate PlaybackState into its own class.
    It will be used by both the waveform preview and detail components.

---
+++ src/main/java/org/deepsymmetry/beatlink/data/PlaybackState.java
@@ -0,0 +1,35 @@
+package org.deepsymmetry.beatlink.data;
+
+/**
+ * Captures the playback state of a single player that has the track loaded, as an immutable value class.
+ */
+public class PlaybackState {
+
+    /**
+     * The player number whose playback state this represents.
+     */
+    public final int player;
+
+    /**
+     * The current playback position of the player in milliseconds.
+     */
+    public final long position;
+
+    /**
+     * Whether the player is actively playing the track.
+     */
+    public final boolean playing;
+
+    /**
+     * Create an instance to represent a particular playback state.
+     *
+     * @param player the player number whose playback state this represents
+     * @param position the current playback position in milliseconds
+     * @param playing whether the player is actively playing the track
+     */
+    public PlaybackState(int player, long position, boolean playing) {
+        this.player = player;
+        this.position = position;
+        this.playing = playing;
+    }
+}
