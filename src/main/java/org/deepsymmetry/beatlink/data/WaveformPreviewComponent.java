b0130de6e12b32a9659e14dfed35a82d31686166
James Elliott <james@deepsymmetry.org>
2019-02-10 20:37:40 -0600

    Don't repaint if position hasn't actually changed.

--- src/main/java/org/deepsymmetry/beatlink/data/WaveformPreviewComponent.java
+++ src/main/java/org/deepsymmetry/beatlink/data/WaveformPreviewComponent.java
@@ -260,12 +260,12 @@ public class WaveformPreviewComponent extends JComponent {
             }
 
             // Also refresh where the specific marker was moved from and/or to.
-            if (oldState != null) {
+            if (oldState != null && (newState == null || newState.position != oldState.position)) {
                 final int left = Math.max(0, Math.min(width, millisecondsToX(oldState.position) - 6));
                 final int right = Math.max(0, Math.min(width, millisecondsToX(oldState.position) + 6));
                 delegatingRepaint(left, 0, right - left, getHeight());
             }
-            if (newState != null) {
+            if (newState != null && (oldState == null || newState.position != oldState.position)) {
                 final int left = Math.max(0, Math.min(width, millisecondsToX(newState.position) - 6));
                 final int right = Math.max(0, Math.min(width, millisecondsToX(newState.position) + 6));
                 delegatingRepaint(left, 0, right - left, getHeight());
