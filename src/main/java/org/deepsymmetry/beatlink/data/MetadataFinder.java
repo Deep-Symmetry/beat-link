aad37758f16276070ab0b42c6c56b9c754449e9a
Christopher Prepos <cprepos@18:3e:ef:da:5b:ca.home>
2024-05-07 23:31:27 -0400

    more informative check

--- src/main/java/org/deepsymmetry/beatlink/data/MetadataFinder.java
+++ src/main/java/org/deepsymmetry/beatlink/data/MetadataFinder.java
@@ -541,7 +541,7 @@ public class MetadataFinder extends LifecycleParticipant {
         if (mediaMounts.add(slot)) {
             deliverMountUpdate(slot, true);
         }
-        if (!mediaDetails.containsKey(slot) && !VirtualRekordbox.getInstance().isRunning()) {
+        if (!mediaDetails.containsKey(slot) && !VirtualCdj.getInstance().inOpusQuadCompatibilityMode()) {
             try {
                 VirtualCdj.getInstance().sendMediaQuery(slot);
             } catch (Exception e) {
